853a19caea0724566afc740dda9c17093b88194f
Matthias Gallant <matthias@gallant.be>
2020-01-03 13:43:39 +0100

    Initial Commit

--- build/CMakeFiles/pilight_shared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libpilight.pdb"
-  "libpilight.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/pilight_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
